084254c4056c5d2300b9750bfad3a9b7bec9c74d
Sreedevi <sreedevi@luminartechnolab.com>
2022-06-21 11:30:04 +0530

    commit

--- lib/Splash.dart
+++ lib/Splash.dart
@@ -9,24 +9,34 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            // Image(
-            //   image: AssetImage("assets/images/paintbucket.png"),
-            //   height: 120,
-            //   width: 120,
-            // ),
-            Icon(Icons.map,size: 50,color: Colors.yellow,),
-            Text(
-              "My App",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue),
-            ),
-          ],
+      body: Container(
+        height: double.infinity,
+        width: double.maxFinite,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage('https://cdn.pixabay.com/photo/2016/02/23/07/37/wall-1217083__340.jpg'),
+            fit: BoxFit.cover
+          ),
+        ) ,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              // Image(
+              //   image: AssetImage("assets/images/paintbucket.png"),
+              //   height: 120,
+              //   width: 120,
+              // ),
+              Icon(Icons.map,size: 50,color: Colors.yellow,),
+              Text(
+                "My App",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue),
+              ),
+            ],
+          ),
         ),
       ),
     );
